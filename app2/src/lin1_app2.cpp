4f1bce08d4aff8c2786b1955b665c5057d27ecf4
Karel Seeuwen <karepon@gmail.com>
2024-10-08 15:59:08 +0900

    Next commit

---
+++ app2/src/lin1_app2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lin1_app2.cpp
+// Author      : Karel Seeuwen
+// Version     :
+// Copyright   : No Copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
